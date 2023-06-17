98acd93ac58b76a5db41ac469da1ba2b8449b7b5
Daniel Bachler <daniel@danielbachler.de>
2023-06-17 15:11:59 +0200

    add setup steps to link config and env

--- setup.sh
+++ setup.sh
@@ -3,7 +3,34 @@ set -e
 
 bootstrap() {
     has_nu_scripts || install_nu_scripts
+    has_env_file_link || link_env_file
+    has_config_file_link || link_config_file
 }
+
+has_config_file_link() {
+    if [ -f "$HOME/.config/nushell/config.nu" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+link_config_file() {
+    ln -s "$HOME/nu/config.nu" "$HOME/.config/nushell/config.nu"
+}
+
+has_env_file_link() {
+    if [ -f "$HOME/.config/nushell/env.nu" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+link_env_file() {
+    ln -s "$HOME/nu/env.nu" "$HOME/.config/nushell/env.nu"
+}
+
 
 has_nu_scripts() {
     if [ -d "$HOME/nu_scripts" ]; then
